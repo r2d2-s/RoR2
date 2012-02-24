25367e660678ebe27652d9f0cc751581927ddadf
Anna Gonchar <anna.gonchar@e-legion.com>
2012-02-24 17:05:49 +0400

    initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoR2::Application.config.secret_token = '7b24f99369665ff236f8e2291dafc34d0dd6fd94207f8b2ae7c0241774bde7dbbe33494d70c42b04c65e3a18d05b1beaf85477905d750edee9e03d7b35908e83'
